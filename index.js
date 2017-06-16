78735baed3065436c2cc81ce21952c3a6be713e5
Jens Lind <jens@jensl.se>
2017-06-16 19:05:07 +0200

    Added opts

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ var stream = require('stream')
 var through = require('through2')
 
 var drupalBreakpointsScss = {}
+var defaultOpts = {
+  vars: true,
+  map: false,
+  mapName: 'drupal-breakpoints',
+  varsPrefix: ''
+}
 
-function jsonToScssVars (obj, varPrefix) {
+function jsonToScssVars (obj, opts) {
   var scssVars = ''
 
   for (var i in obj) {
-    scssVars += '$' + varPrefix + obj[i].label + ': \'' + obj[i].mediaQuery + '\';\n'
+    scssVars += '$' + opts.varsPrefix + obj[i].label + ': \'' + obj[i].mediaQuery + '\';\n'
   }
 
   return scssVars
 }
 
-function jsonToScssMap (obj, mapName) {
-  var scssMap = '$' + mapName + ': (\n'
+function jsonToScssMap (obj, opts) {
+  var scssMap = '$' + opts.mapName + ': (\n'
 
   for (var i in obj) {
     scssMap += '  ' + obj[i].label + ': \'' + obj[i].mediaQuery + '\',\n'
@@ -28,12 +34,22 @@ function jsonToScssMap (obj, mapName) {
   return scssMap
 }
 
-drupalBreakpointsScss.read = function (path) {
+function generateScss (breakpoints, opts) {
+  var opts = Object.assign(defaultOpts, opts)
+
+  var output = ''
+  if (opts.vars) output += jsonToScssVars(breakpoints, opts) + '\n'
+  if (opts.map) output += jsonToScssMap(breakpoints, opts) + '\n'
+
+  return output
+}
+
+drupalBreakpointsScss.read = function (path, opts = {}) {
   var rs = stream.Readable()
   var breakpoints = YAML.load(path)
 
   rs._read = function () {
-    rs.push(jsonToScssVars(breakpoints))
+    rs.push(generateScss(breakpoints, opts))
     rs.push(null)
   }
 
@@ -45,11 +61,11 @@ drupalBreakpointsScss.write = function (path) {
   return scssFile
 }
 
-drupalBreakpointsScss.ymlToScss = function (varPrefix = '', mapName = 'drupal-breakpoints') {
+drupalBreakpointsScss.ymlToScss = function (opts = {}) {
   return through.obj(function (file, enc, cb) {
     var content = file.contents.toString('utf8')
-    var parsedYaml = YAML.parse(content)
-    file.contents = new Buffer(String(jsonToScssVars(parsedYaml, varPrefix) + '\n' + jsonToScssMap(parsedYaml, mapName)))
+    var breakpoints = YAML.parse(content)
+    file.contents = new Buffer(String(generateScss(breakpoints, opts)))
     cb(null, file)
   })
 }
